ac9ea6e0ae8784ec106785789cc1040f479f0933
midays <midays@redhat.com>
2022-05-08 18:28:15 +0300

    add code check

--- cypress/integration/tests/sanity.test.ts
+++ cypress/integration/tests/sanity.test.ts
@@ -32,9 +32,9 @@ describe('Automated tests to do direct and indirect migrations and Basic Pipelin
       cy.log("Login")
       login();
       cy.log(`"setup source cluster: ${sourceCluster}"`)
-      cy.exec(`"${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}"`, { timeout: 200000 });
+      cy.exec(`"${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
       cy.log(`"setup target cluster: "${targetCluster}""`)
-      cy.exec(`"${configurationScript}" setup_target_cluster ${Data.namespaceList} "${targetCluster}"`, { timeout: 200000 });
+      cy.exec(`"${configurationScript}" setup_target_cluster ${Data.namespaceList} "${targetCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
       plan.create(Data);
       plan.execute(Data);
       if (`${migrationType}` == 'Rollover indirect migration and then migrate' ||
