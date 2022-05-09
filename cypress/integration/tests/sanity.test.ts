fa6e202217a4b7a1f497cd4e8677f54723910c0e
midays <midays@redhat.com>
2022-05-09 12:00:11 +0300

    run script

--- cypress/integration/tests/sanity.test.ts
+++ cypress/integration/tests/sanity.test.ts
@@ -32,6 +32,7 @@ describe('Automated tests to do direct and indirect migrations and Basic Pipelin
       cy.log("Login")
       login();
       cy.log(`"setup source cluster: ${sourceCluster}"`)
+      cy.exec('echo "HELLO WORLD" > /tmp/testfile')
       cy.log(`"Executing: [${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}]"`)
       cy.exec(`"${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
       cy.log(`"setup target cluster: "${targetCluster}""`)
