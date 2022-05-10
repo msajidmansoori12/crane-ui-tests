d850ebb990d80d093d4f4847984de6b400e1c4fb
M Sajid Mansoori <mmansoor@localhost.localdomain>
2022-05-10 18:06:51 +0530

    Fix path

--- cypress/integration/tests/sanity.test.ts
+++ cypress/integration/tests/sanity.test.ts
@@ -6,7 +6,7 @@ import { Plan } from '../models/plan'
 
 const sourceCluster = Cypress.env('sourceCluster');
 const targetCluster = Cypress.env('targetCluster');
-const configurationScript = "../../utils/configuration_script.sh"
+const configurationScript = "cypress/utils/configuration_script.sh"
 
 describe('Automated tests to do direct and indirect migrations and Basic Pipeline Status Verification', () => {
   const plan = new Plan();
