35bf8674faef863dc61a58c49d0a59029b29c0b9
midays <midays@redhat.com>
2022-05-10 12:02:58 +0300

    bash -ex

--- cypress/integration/tests/sanity.test.ts
+++ cypress/integration/tests/sanity.test.ts
@@ -32,8 +32,8 @@ describe('Automated tests to do direct and indirect migrations and Basic Pipelin
       login();
       // cy.exec("bash -ex ls /var/lib/jenkins/workspace/mtc-ui-test-runner-updated/crane-ui-tests/cypress/utils")
       // cy.exec(`"bash -ex cat ${configurationScript}"`)
-      cy.exec(`"${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
-      cy.exec(`"${configurationScript}" setup_target_cluster ${Data.namespaceList} "${targetCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
+      cy.exec(`"bash -ex ${configurationScript}" setup_source_cluster ${Data.namespaceList} "${sourceCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
+      cy.exec(`"bash -ex ${configurationScript}" setup_target_cluster ${Data.namespaceList} "${targetCluster}"`, { timeout: 200000 }).its('code').should('eq', 0);;
       plan.create(Data);
       plan.execute(Data);
       if (`${migrationType}` == 'Rollover indirect migration and then migrate' ||
